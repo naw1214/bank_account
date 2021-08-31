dd5cbecfcef95a331bd85b2b0514fda989a76127
Nathan Wright <nathanw1214@gmail.com>
2021-08-31 13:43:35 -0600

    second commit

--- bank_account.py
+++ bank_account.py
@@ -1,7 +1,9 @@
 class BankAccount:
+    accounts = []
     def __init__(self, int_rate, balance): 
-        self.int_rate = 0
-        self.balance = 0
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
     # your code here! (remember, instance attributes go here)
     # don't worry about user info here; we'll involve the User class soon
     def deposit(self, amount):
@@ -9,20 +11,31 @@ class BankAccount:
         return self
         
     def withdraw(self, amount):
-        self.balance -= amount
+        if self.balance - amount >= 0:
+            self.balance -= amount
+        else: 
+            print('You have insuffcient funds')
+            self.balance -= 5
         return self
     
-    def yield_interest(self, amount):
-        self.int_rate = amount
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
         return self
     
     def display_account_info(self):
-        print(f'{self.balance} at {self.int_rate}')
+        print(f'Balance: {self.balance}')
         return self
     
+    @classmethod
+    def print_info(cls):
+        for account in cls.accounts:
+            account.display_account_info()
 
-eugine = BankAccount(0,0)
-thor = BankAccount(0,0)
+savings = BankAccount(0.5,3000)
+checking = BankAccount(0.2,900)
 
-eugine.deposit(100).deposit(100).deposit(200).withdraw(100).yield_interest(.06).display_account_info()
-thor.deposit(200).deposit(300).withdraw(50).withdraw(150).withdraw(220).withdraw(450).yield_interest(.10).display_account_info()
+savings.deposit(100).deposit(100).deposit(200).withdraw(100).yield_interest().display_account_info()
+checking.deposit(200).deposit(300).withdraw(50).withdraw(150).withdraw(220).withdraw(450).yield_interest().display_account_info()
+
+BankAccount.print_info()
